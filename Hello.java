c658ce68fb8aada7aab411acd1b6aa6bf763639f
Ubuntu <civerson@gmail.com>
2021-02-08 09:39:41 +0000

    changed Hello Java program message output to screen

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello {
     public static void main(String[] args) {
-      System.out.println("This java app has been updated to show a change for SCM polling");
+      System.out.println("This java app has been updated to trigger a change event for SCM polling");
     }
   }
 
