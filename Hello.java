c2b573d0a59bcf44a1106dca223fa1a37806d471
Ubuntu <civerson@gmail.com>
2021-02-08 08:10:44 +0000

    Changed the message printed by Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello {
     public static void main(String[] args) {
-      System.out.println("This is a java app created using Docker");
+      System.out.println("This java app has been updated to show a change for SCM polling");
     }
   }
 
